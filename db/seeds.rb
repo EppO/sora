3780be430f2982c2a50fac6080650abef74b09b9
Florent Monbillard <f.monbillard@gmail.com>
2011-10-31 17:50:18 -0400

    clean up and added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+callers = %w( 0142123456 0209872453 0145279865 0164521192 0512333316 0149730211 0309730376 0509736676 0129834376 )
+called = %w( 0145081223 0148341315 0164272845 0160981432 0419723322 0148341315 0168673315 0155351316 0131311315 0248341944 )
+dates = ((Date.today - 3.month)..Date.today).to_a
+call_types = [ "answer", "originate" ]
+
+for i in 500..rand(10000)+500 do
+  call_date = dates[rand(dates.size)]
+  call_start = call_date.to_datetime + rand(24).hours + rand(60).minutes + rand(60).seconds
+  call_end = call_start + rand(60).minutes + rand(60).seconds
+  duration = call_end - call_start
+  Call.create(:call_id => rand(100)*rand(100)*rand(100)*rand(100), :caller_id => callers[rand(callers.size)], :called_id => called[rand(called.size)], 
+              :call_type => call_types[rand(call_types.size)], :call_start => call_start, :call_end => call_end, 
+              :call_date => call_date, :call_hour => rand(24), :call_duration => duration)
+end
